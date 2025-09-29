8297dac81fdae3fe54f563729b6c213c8f32e38b
Wesley Liu <152113348+wesleyyliu@users.noreply.github.com>
2025-09-29 16:56:00 -0400

    first commit

--- src/analysis.js
+++ src/analysis.js
@@ -2,6 +2,9 @@
  * [TODO] Step 0: Import the dependencies, fs and papaparse
  */
 
+const fs = require('fs');
+const papaparse = require('papaparse');
+
 /**
  * [TODO] Step 1: Parse the Data
  *      Parse the data contained in a given file into a JavaScript objectusing the modules fs and papaparse.
@@ -9,7 +12,11 @@
  * @param {string} filename - path to the csv file to be parsed
  * @returns {Object} - The parsed csv file of app reviews from papaparse.
  */
-function parseData(filename) {}
+function parseData(filename) {
+    const data = fs.readFileSync(filename, 'utf8');
+    const result = papaparse.parse(data, { header: true });
+    return result;
+}
 
 /**
  * [TODO] Step 2: Clean the Data
@@ -26,7 +33,45 @@ function parseData(filename) {}
  * @param {Object} csv - a parsed csv file of app reviews
  * @returns {Object} - a cleaned csv file with proper data types and removed null values
  */
-function cleanData(csv) {}
+function cleanData(csv) {
+    for (const key in csv.data[0]) {
+        if (key === 'user_gender') {
+            continue;
+        }
+        csv.data = csv.data.filter(entry => entry[key] !== null && entry[key] !== '' && entry[key] !== undefined);
+    }
+    csv.data = csv.data.map(mergeUser);
+    csv.data = csv.data.map(convertTypes);
+    return csv.data;
+}
+
+function mergeUser(entry) {
+    const new_entry = { 
+        ...entry, 
+        user: {
+            user_id: parseInt(entry.user_id),
+            user_age: parseInt(entry.user_age),
+            user_country: entry.user_country,
+            user_gender: entry.user_gender,
+        }
+    };
+    delete new_entry.user_id;
+    delete new_entry.user_age;
+    delete new_entry.user_country;
+    delete new_entry.user_gender;
+    return new_entry;
+}
+
+function convertTypes(entry) {
+    return {
+        ...entry,
+        num_helpful_votes: parseInt(entry.num_helpful_votes),
+        rating: parseFloat(entry.rating),
+        review_date: new Date(entry.review_date),
+        review_id: parseInt(entry.review_id),
+        verified_purchase: entry.verified_purchase === 'True',
+    };
+}
 
 /**
  * [TODO] Step 3: Sentiment Analysis
@@ -38,7 +83,15 @@ function cleanData(csv) {}
  * @returns {string} - 'positive' if rating is greater than 4, negative is rating is below 2,
  *                      and neutral if it is between 2 and 4.
  */
-function labelSentiment({ rating }) {}
+function labelSentiment({ rating }) {
+    if (rating > 4) {
+        return 'positive';
+    }
+    if (rating < 2) {
+        return 'negative';
+    }
+    return 'neutral';
+}
 
 /**
  * [TODO] Step 3: Sentiment Analysis by App
@@ -48,7 +101,26 @@ function labelSentiment({ rating }) {}
  * @param {Object} cleaned - the cleaned csv data
  * @returns {{app_name: string, positive: number, neutral: number, negative: number}[]} - An array of objects, each summarizing sentiment counts for an app
  */
-function sentimentAnalysisApp(cleaned) {}
+function sentimentAnalysisApp(cleaned) {
+    const sentiments = cleaned.map(entry => ({
+        ...entry,
+        sentiment: labelSentiment({ rating: entry.rating })
+    }));
+    
+    const ans = {};
+    sentiments.forEach(entry => {
+        if (!ans[entry.app_name]) {
+            ans[entry.app_name] = {
+                app_name: entry.app_name,
+                positive: 0,
+                neutral: 0,
+                negative: 0
+            };
+        }
+        ans[entry.app_name][entry.sentiment]++;
+    });
+    return Object.values(ans);
+}
 
 /**
  * [TODO] Step 3: Sentiment Analysis by Language
@@ -58,7 +130,26 @@ function sentimentAnalysisApp(cleaned) {}
  * @param {Object} cleaned - the cleaned csv data
  * @returns {{lang_name: string, positive: number, neutral: number, negative: number}[]} - An array of objects, each summarizing sentiment counts for a language
  */
-function sentimentAnalysisLang(cleaned) {}
+function sentimentAnalysisLang(cleaned) {
+    const sentiments = cleaned.map(entry => ({
+        ...entry,
+        sentiment: labelSentiment({ rating: entry.rating })
+    }));
+    
+    const ans = {};
+    sentiments.forEach(entry => {
+        if (!ans[entry.review_language]) {
+            ans[entry.review_language] = {
+                lang_name: entry.review_language,
+                positive: 0,
+                neutral: 0,
+                negative: 0
+            };
+        }
+        ans[entry.review_language][entry.sentiment]++;
+    });
+    return Object.values(ans);
+}
 
 /**
  * [TODO] Step 4: Statistical Analysis
@@ -75,7 +166,48 @@ function sentimentAnalysisLang(cleaned) {}
  * @returns {{mostReviewedApp: string, mostReviews: number, mostUsedDevice: String, mostDevices: number, avgRating: float}} -
  *          the object containing the answers to the desired summary statistics, in this specific format.
  */
-function summaryStatistics(cleaned) {}
+function summaryStatistics(cleaned) {
+    const agg = {};
+    cleaned.forEach(entry => {
+        if (!agg[entry.app_name]) {
+            agg[entry.app_name] = {
+                app_name: entry.app_name,
+                num_reviews: 0,
+                devices: {},
+                total_rating: 0.0,
+            };
+        }
+        agg[entry.app_name].num_reviews++;
+        agg[entry.app_name].total_rating += entry.rating;
+        if (!agg[entry.app_name].devices[entry.device_type]) {
+            agg[entry.app_name].devices[entry.device_type] = 0;
+        }
+        agg[entry.app_name].devices[entry.device_type]++;
+    });
+    let most_reviewed_entry = null;
+    let most_reviews = 0;
+    for (const app_name in agg) {
+        if (agg[app_name].num_reviews > most_reviews) {
+            let most_devices = 0;
+            let most_used_device = null;
+            for (const device_type in agg[app_name].devices) {
+                if (agg[app_name].devices[device_type] > most_devices) {
+                    most_used_device = device_type;
+                    most_devices = agg[app_name].devices[device_type];
+                }
+            }
+            most_reviewed_entry = {
+                mostReviewedApp: app_name,
+                mostReviews: agg[app_name].num_reviews,
+                mostUsedDevice: most_used_device,
+                mostDevices: most_devices,
+                avgRating: agg[app_name].total_rating / agg[app_name].num_reviews,
+            }
+            most_reviews = agg[app_name].num_reviews;
+        }
+    }
+    return most_reviewed_entry;
+}
 
 /**
  * Do NOT modify this section!
